cd5b98d9250faf5bb7de9d427e81887f48fb8bd2
khrystynadol <khrystyna.dolynska.kn.2021@lpnu.ua>
2025-03-14 22:07:13 +0200

    Mobile app design and flow chart added

--- mobile_app/bottom_toolbar.py
+++ mobile_app/bottom_toolbar.py
@@ -1,18 +1,56 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivymd.uix.relativelayout import MDRelativeLayout
-from kivymd.uix.button import MDFloatingActionButton
-from kivymd.uix.bottomnavigation import MDBottomNavigation, MDBottomNavigationItem
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.button import MDIconButton
+from kivy.uix.filechooser import FileChooserIconView
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.label import MDLabel
 
+# Window.size = (400, 600)
 Window.size = (300, 600)
 
 # KV Language for UI Layout
 screen_helper = """
 BoxLayout:
     orientation: 'vertical'
+    # md_bg_color: 0.992, 0.957, 0.863, 1
+    
+    MDTopAppBar:
+        title: "Logo"
+        anchor_title: "left"
+        height: dp(20)
+        left_action_items: []
+        right_action_items: [["cog", lambda x: app.on_settings_pressed()]]
+        elevation: 3
+        md_bg_color: 0.427, 0.592, 0.451, 1
+        # theme_text_color: "Custom"
+        # text_color: 0.992, 0.957, 0.863, 1
+    
+    # MDBoxLayout:
+    #     size_hint_y: None
+    #     height: dp(40)
+    #     padding: dp(10)
+    #     spacing: dp(10)
+    # 
+    #     MDLabel:
+    #         text: "Logo"
+    #         font_style: "Subtitle2"
+    #         # size_hint_x: 0.9
+    #         valign: "center"
+    #         halign: "left"
+    #         padding: dp(10), 0, 0, 0
+    # 
+    #     MDIconButton:
+    #         icon: "cog"
+    #         user_font_size: "24sp"
+    #         theme_text_color: "Primary"
+    #         on_release: app.on_settings_pressed()
+    #         valign: "center"
+    #         halign: "right"
+    #         padding: 0, dp(15), dp(10), 0
 
     MDScreenManager:
         id: screen_manager
@@ -20,7 +58,7 @@ BoxLayout:
         HomeScreen:
         FavouriteScreen:
         NotificationScreen:
-        ProfileScreen:
+        ProfileScreen:   
 
     MDRelativeLayout:
         size_hint_y: None
@@ -28,14 +66,16 @@ BoxLayout:
 
         MDBottomNavigation:
             id: bottom_nav
-            panel_color: 1, 1, 1, 1
-            selected_color_background: 0, 0, 1, 0.2
-            text_color_active: 0, 0, 1, 1
+            selected_color_background: 0.427, 0.592, 0.451, 0.2
+            text_color_active: 0.427, 0.592, 0.451, 1
+            # text_color_active: 0.047, 0.231, 0.180, 1
+            font_style: "Caption"
 
             MDBottomNavigationItem:
                 name: 'home'
                 text: 'Home'
                 icon: 'home'
+                font_style: "Caption"
                 on_tab_press: app.switch_screen('home')
 
             MDBottomNavigationItem:
@@ -48,7 +88,7 @@ BoxLayout:
                 name: 'microphone'
                 text: ''
                 icon: ''
-                disabled: True  # Spacer for the microphone button
+                disabled: True
 
             MDBottomNavigationItem:
                 name: 'notifications'
@@ -61,23 +101,86 @@ BoxLayout:
                 text: 'Profile'
                 icon: 'account'
                 on_tab_press: app.switch_screen('profile')
-
+                
         MDFloatingActionButton:
             icon: "microphone"
+            # theme_icon_color: "Custom"
+            # icon_color: 0.992, 0.957, 0.863, 1
             size_hint: None, None
-            size: dp(75), dp(75)
-            elevation: 12
+            # size: dp(75), dp(75)
+            elevation: 3
             pos_hint: {"center_x": 0.5, "center_y": 1.1}
-            md_bg_color: 0, 0, 1, 1
+            md_bg_color: 0.047, 0.231, 0.180, 1
             on_release: app.on_microphone_pressed()
 
 <HomeScreen>:
     name: "home"
+    
     MDBoxLayout:
         orientation: "vertical"
+        padding: dp(20)
+                
+        MDSeparator:
+            height: dp(1)
+        
+        MDBoxLayout:
+            width: dp(500)
+            size_hint_y: None
+            orientation: 'horizontal'
+            
+            # MDIconButton:
+            #     icon: "folder"
+            #     user_font_size: "100sp" 
+            #     theme_text_color: "Primary"
+            #     on_release: app.on_upload_pressed()
+
+            MDIconButton:
+                icon: "folder"
+                font_size: "50sp"
+                # theme_text_color: "Primary"
+                halign: "center"
+                theme_icon_color: "Custom"
+                icon_color: 0.047, 0.231, 0.180, 1
+                on_release: app.on_upload_pressed()
+
+            MDLabel:
+                text: "Upload File"
+                size_hint_x: 0.5
+                on_ref_press: app.on_upload_pressed()
+                valign: "center"
+                padding: 0, 5, 0, 0
+                theme_text_color: "Custom"
+                text_color: 0.047, 0.231, 0.180, 1
+                
+        # MDBoxLayout:
+        #     orientation: 'vertical'
+        #     size_hint_y: None
+        #     height: dp(100)
+        #     spacing: dp(10)
+        #     pos_hint: {"center_x": 0.5}
+        # 
+        #     MDIconButton:
+        #         icon: "folder"
+        #         user_font_size: "60sp"  # Bigger folder icon
+        #         pos_hint: {"center_x": 0.5}
+        #         on_release: app.on_upload_pressed()
+        # 
+        #     MDLabel:
+        #         text: "Upload File"
+        #         halign: "center"
+        #         theme_text_color: "Primary"
+        #         font_style: "H6"
+        #         on_ref_press: app.on_upload_pressed()
+
+        MDSeparator:
+            height: dp(1)
+
         MDLabel:
             text: "Recent Audios will be displayed here."
             halign: "center"
+            theme_text_color: "Secondary"
+            font_style: "Subtitle1"
+            padding: dp(10)
 
 <FavouriteScreen>:
     name: "favourites"
@@ -104,6 +207,7 @@ BoxLayout:
             halign: "center"
 """
 
+
 class HomeScreen(MDScreen):
     pass
 
@@ -122,14 +226,26 @@ class ProfileScreen(MDScreen):
 
 class DemoApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.material_style = "M3"
         return Builder.load_string(screen_helper)
 
     def switch_screen(self, screen_name):
-        self.root.ids.screen_manager.current = screen_name
+        screen_manager = self.root.ids.screen_manager
+        screen_manager.current = screen_name
 
-    def on_microphone_pressed(self):
-        print("Microphone button pressed")
+    def on_upload_pressed(self):
+        # Open the file chooser to select an audio file
+        content = FileChooserIconView()
+        content.bind(on_selection=lambda *x: self.select_file(content))
+        self.popup = Popup(title="Select Audio File", content=content, size_hint=(0.8, 0.8))
+        self.popup.open()
+
+    def select_file(self, filechooser):
+        selected_file = filechooser.selection
+        if selected_file:
+            print(f"Selected file: {selected_file[0]}")
+            # You can add code here to handle the audio file (e.g., uploading or playing it)
+        self.popup.dismiss()
 
 
 if __name__ == "__main__":
--- mobile_app/main.py
+++ mobile_app/main.py
@@ -1,160 +1,52 @@
-from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
 from kivy.lang import Builder
-from kivy.core.window import Window
 
-Window.size = (300, 500)
-
-screen_helper = """
-Screen:
-    BoxLayout:
-        orientation: 'vertical'
-        MDTopAppBar:
-            title: 'Demo Application'
-            left_action_items: [["menu", lambda x: app.navigation_draw()]]
-            right_action_items: [["dots-vertical", lambda x: app.callback()], ["clock", lambda x: app.callback_2()]]
-            elevation:5
-
-        MDLabel:
-            text: 'hello world'
-            halign: 'center'
-"""
+from kivymd.app import MDApp
 
 
-class DemoApp(MDApp):
+class Test(MDApp):
 
     def build(self):
-        self.theme_cls.primary_palette = "Red"
-        screen = Builder.load_string(screen_helper)
+        self.theme_cls.material_style = "M3"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(
+            '''
+MDScreen:
 
-        return screen
+    MDBottomNavigation:
+        #panel_color: "#eeeaea"
+        selected_color_background: "orange"
+        text_color_active: "lightgrey"
 
-    def navigation_draw(self):
-        print("Navigation")
+        MDBottomNavigationItem:
+            name: 'screen 1'
+            text: 'Mail'
+            icon: 'gmail'
+            badge_icon: "numeric-10"
 
+            MDLabel:
+                text: 'Mail'
+                halign: 'center'
 
-if __name__ == "__main__":
-    DemoApp().run()
+        MDBottomNavigationItem:
+            name: 'screen 2'
+            text: 'Twitter'
+            icon: 'twitter'
+            badge_icon: "numeric-5"
 
-# from kivymd.app import MDApp
-# from kivymd.uix.label import MDLabel
-# from kivymd.uix.screen import Screen
-# from kivy.lang import Builder
-#
-# # builder method
-# helper = """
-# Screen:
-#     name:'About us'
-#     BoxLayout:
-#         orientation:'vertical'
-#         MDToolbar:
-#             title:'Demo'
-#             left_action_items:[['menu',lambda x: app.navigation_draw()]]
-#             right_action_items:[['logout',lambda x: app.navigation_draw()]]
-#             elevation:10
-#             md_bg_color: 0,0,100/255,1
-#
-#         MDLabel:
-#             text:"hi"
-#             halign:'center'
-#         MDBottomAppBar:
-#             MDToolbar:
-#                 title:'Bottom'
-#                 left_action_items:[['menu',lambda x: app.navigation_draw()]]
-#                 elevation:10
-#                 md_bg_color: 0,0,100/255,1
-# """
-#
-#
-# class Demo(MDApp):
-#
-#     def build(self):
-#         screen = Builder.load_string(helper)
-#         return screen
-#
-#     # lambda Function
-#     def navigation_draw(self):
-#         print("NavBar")
-#
-#
-# if __name__ == "__main__":
-#     Demo().run()
+            MDLabel:
+                text: 'Twitter'
+                halign: 'center'
 
-# from kivy.lang import Builder
-# from kivymd.app import MDApp
-# from kivymd.uix.boxlayout import MDBoxLayout
-# from kivymd.uix.button import MDFloatingActionButton
-# from kivymd.uix.screen import Screen
-# from kivymd.uix.toolbar import MDBottomAppBar
-#
-# KV = """
-# Screen:
-#     MDBottomAppBar:
-#         MDToolbar:
-#             type: "bottom"
-#             mode: "center"
-#             icon: "microphone"
-#             on_action_button: app.on_microphone_pressed()
-#             left_action_items: [["home", lambda x: app.on_home_pressed()], ["checkbox-marked-circle", lambda x: app.on_selected_pressed()]]
-#             right_action_items: [["bell", lambda x: app.on_notifications_pressed()], ["account", lambda x: app.on_personal_info_pressed()]]
-# """
-#
-#
-# class MyApp(MDApp):
-#     def build(self):
-#         return Builder.load_string(KV)
-#
-#     def on_microphone_pressed(self):
-#         print("Microphone pressed")
-#
-#     def on_home_pressed(self):
-#         print("Home pressed")
-#
-#     def on_selected_pressed(self):
-#         print("Selected item pressed")
-#
-#     def on_notifications_pressed(self):
-#         print("Notifications pressed")
-#
-#     def on_personal_info_pressed(self):
-#         print("Personal Info pressed")
+        MDBottomNavigationItem:
+            name: 'screen 3'
+            text: 'LinkedIN'
+            icon: 'linkedin'
+
+            MDLabel:
+                text: 'LinkedIN'
+                halign: 'center'
+'''
+        )
 
 
-# from kivy.lang import Builder
-# from kivymd.app import MDApp
-#
-# KV = '''
-# MDBoxLayout:
-#     MDBottomAppBar:
-#         MDTopAppBar:
-#             title: "Title"
-#             icon: "git"
-#             type: "bottom"
-#             left_action_items: [["menu", lambda x: x]]
-#             mode: "end"
-# '''
-#
-#
-# class MyApp(MDApp):
-#     def build(self):
-#         return Builder.load_string(KV)
-#
-#
-# if __name__ == "__main__":
-#     MyApp().run()
-
-# import kivy
-#
-# from kivy.app import App
-# from kivy.uix.label import Label
-#
-# kivy.require('2.3.0')
-#
-#
-# class MyApp(App):
-#     def build(self):
-#         return Label(text='Hello world')
-#
-#
-# if __name__ == '__main__':
-#     MyApp().run()
+Test().run()
